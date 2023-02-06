41517642c07f861e47d7351b677b7e42d558bb25
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2023-02-06 17:38:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Token:"MTA1NTI0NjMxMzYzMTAxMDg3Nw.GFUyV2.T3FnO1th_U2G-ZQnrMsDzwjHOBWSpzFuuKPODA",
+    Token:"X",
     clientID : "1055246313631010877",
     guildID : "1055247534408355840",
     DatabaseURL : "mongodb+srv://ray:r2WuDpIi4J9kyTad@vampxp.oib8zql.mongodb.net/?retryWrites=true&w=majority",
