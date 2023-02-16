11313c45b80b9a6adc40cea71c64526915e9a53b
1tzRayz <106563842+1tzRayz@users.noreply.github.com>
2023-02-16 09:27:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     Token:"X",
-    clientID : "1055246313631010877",
+    clientID : "X",
     guildID : "1055247534408355840",
-    DatabaseURL : "mongodb+srv://ray:r2WuDpIi4J9kyTad@vampxp.oib8zql.mongodb.net/?retryWrites=true&w=majority",
+    DatabaseURL : "X",
 } 
